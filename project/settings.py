fcf360d9003f019f895ebc851cc6e52ef625ae15
Rokon1992 <rokonsikkdar01@gmail.com>
2026-08-22 16:14:23 +0300

    prepare django settings for render

--- project/settings.py
+++ project/settings.py
@@ -10,10 +10,15 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
+
+import os
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 
 # Quick-start development settings - unsuitable for production
